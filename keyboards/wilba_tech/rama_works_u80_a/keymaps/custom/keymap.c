8c21303f8fb0a9a715e273efc041fbb551c57386
jd <johndetora@gmail.com>
2023-02-19 15:30:39 -0500

    SUCCESS

--- keyboards/wilba_tech/rama_works_u80_a/keymaps/custom/keymap.c
+++ keyboards/wilba_tech/rama_works_u80_a/keymaps/custom/keymap.c
@@ -1,22 +1,16 @@
 #include QMK_KEYBOARD_H
 
-#define _BASE     0
-#define _MOD     1
-#define _MOD2     2
-#define _MOD3     3
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	  [_BASE] = LAYOUT_all(
-	    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SCRL, KC_PAUS,
+	  [0] = LAYOUT_all(
+	    KC_ESC,  RGB_TOG,   KC_2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SCRL, KC_PAUS,
 	    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_INS,  KC_HOME, KC_PGUP,
 	    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,  KC_END,  KC_PGDN,
 	    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                                     KC_UP,
 	    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, MO(1),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
 
-	  [_MOD] = LAYOUT_all(
+	  [1] = LAYOUT_all(
 	    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,
 	    KC_TRNS, EF_DEC,  EF_INC,  H1_DEC,  H1_INC,  H2_DEC,  H2_INC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BR_DEC,  BR_INC,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,
 	    KC_TRNS, KC_TRNS, KC_TRNS, S1_DEC,  S1_INC,  S2_DEC,  S2_INC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, ES_DEC,  ES_INC,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,
@@ -41,68 +35,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	    KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
-
-
-void keyboard_pre_init_user(void) {
-  setPinOutput(B0);
-  setPinOutput(B1);
-  setPinOutput(B2);
-  setPinOutput(B3);
-  setPinOutput(B4);
+#define ESC_LED_INDEX 70
+bool rgb_matrix_indicators_user(void) {
+    for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+        if (i != ESC_LED_INDEX) rgb_matrix_set_color(i, RGB_OFF);
+    }
+    return true;
 }
-
-// bool led_update_kb(led_t led_state) {
-//     bool res = led_update_user(led_state);
-//     if(res) {
-//         // writePin sets the pin high for 1 and low for 0.
-//         // In this example the pins are inverted, setting
-//         // it low/0 turns it on, and high/1 turns the LED off.
-//         // This behavior depends on whether the LED is between the pin
-//         // and VCC or the pin and GND.
-//         writePin(B0, !led_state.num_lock);
-//         writePin(B1, !led_state.caps_lock);
-//         writePin(B2, !led_state.scroll_lock);
-//         writePin(B3, !led_state.compose);
-//         writePin(B4, !led_state.kana);
-//     }
-//     return res;
-// }
-
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-// 	int led_index = 2;
-//     switch (get_highest_layer(layer_state)) {
-//         case _BASE:
-// 			 rgblight_setrgb (0x00,  0x00, 0xFF);
-//             writePinHigh(B1); 
-// 			writePinLow(B0); 
-// 			rgb_matrix_set_color(led_index, RGB_WHITE);	
-//             break;
-//         case _MOD:
-//             writePinHigh(B0); 
-//             writePinLow(B1);
-//             break;
-// 		default: 
-// 			break;
-  
-// }
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     switch (get_highest_layer(state)) {
-//         case _BASE:
-// 			// rgblight_setrgb (0x00,  0x00, 0xFF);
-//             writePinHigh(B1);
-//             writePinLow(B0);
-	
-//             break;
-//         case _MOD:
-//             writePinHigh(B0); 
-//             writePinLow(B1);
-//             break;
-//         default:
-//             writePinLow(B1);
-//             writePinLow(B0);
-//             break;
-//     }
-// 	return state;
-// }
-
-                            
